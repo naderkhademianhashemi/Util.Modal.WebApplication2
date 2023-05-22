38f2e30261e8d2b9a62586353a34b46194ede5d2
PC 2 <PC 2@DESKTOP-8ELQAGK>
2023-05-22 13:10:56 +0430

    Add project files.

---
+++ Util.Modal.WebApplication2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Util.Modal.WebApplication2.Global" Language="C#" %>
---
+++ Util.Modal.WebApplication2/Pages/NModal.ts
@@ -0,0 +1,22 @@
+﻿class NModal extends Card {
+    constructor($mainPage: JQuery) {
+        super(' ');
+
+        this.$card.appendTo($mainPage);
+
+        var $row = $('<div class="row" />').appendTo(this.$body);
+        var $col4 = $('<div class="col col-4" />').append('&nbsp;').appendTo($row);
+        var $col8 = $('<div class="col col-8 text-left" />').appendTo($row);
+
+        var $actions = $('<div class="display-inline-block" />').appendTo($col4);
+
+
+        Button("NDR", ev => openModal("https://localhost:44380/ErrorPage/Error.aspx")).appendTo($actions.append(' '));
+
+    }
+}
+
+$(function () {
+    var btnNdr = new NModal($("#mainPage"));
+
+})
